9bb2849ffb1858c5041238e5c4ef364fa2b96f70
Vince-lgd <lagardevincent64@gmail.com>
2026-06-07 22:16:07 +0200

    init flutter and create screens login

---
+++ mobile/android/app/src/main/kotlin/com/informya/mobile/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.informya.mobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- mobile/src/screens/ArticlesScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from "react-native";
-
-export default function ArticlesScreen() {
-  return (
-    <View>
-      <Text>Articles</Text>
-    </View>
-  );
-}
